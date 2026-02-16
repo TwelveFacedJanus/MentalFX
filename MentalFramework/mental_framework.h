29d0a7435ad2e23924a4c1744ba0a5e741008b8a
Daniil Ermolaev <twofaced-janus@yandex.ru>
2026-02-16 23:22:20 +0500

    Disable copy & move for MFW_C_Window
    Beauty code. New getters.

--- MentalFramework/mental_framework.h
+++ MentalFramework/mental_framework.h
@@ -2,6 +2,8 @@
 #define MENTAL_FRAMEWORK_H
 
 #include <cstdint>
+
+
 namespace mental_framework
 {
 
@@ -14,8 +16,23 @@ class MFW_C_Window
         MFW_C_Window() = delete;
         ~MFW_C_Window() = delete;
 
-        [[nodiscard]] const uint32_t* getPosition() const { return this->_position; }
-        [[nodiscard]] const uint32_t* getSize() const { return this->_size; }
+        MFW_C_Window(const MFW_C_Window&) = delete;
+        MFW_C_Window& operator=(const MFW_C_Window&) = delete;
+
+        MFW_C_Window(MFW_C_Window&&) = delete;
+        MFW_C_Window& operator=(MFW_C_Window&&) = delete;
+
+
+        [[nodiscard]] const uint32_t*       getSize() const                         { return this->_size; }
+        [[nodiscard]]       uint32_t        getSize(uint8_t index)                  { return (index < 2) ? _size[index] : 0;  }
+
+        [[nodiscard]] const uint32_t*       getPosition() const                     { return this->_position; }
+        [[nodiscard]]       uint32_t        getPosition(uint8_t index)              { return (index < 2) ? _position[index] : 0;  }
+
+                            void            setPosition(uint32_t x, uint32_t y)     { this->_position[0] = x; this->_position[1] = y; }
+                            void            setSize    (uint32_t x, uint32_t y)     { this->_size[0] = x; this->_size[1] = y; }
+
+        [[nodiscard]]       bool            windowShouldClose();
 };
 
 }
